bc8af48953b088ebb15d470adae8d3a7b8cb6fa3
PALLAVI DHAR <pallavi.dhar@gammanalytics.com>
2023-02-21 18:37:28 +0530

    ReferenceDimSuscriberCRMInf.java Add

---
+++ src/main/java/com/gamma/skybase/build/server/etl/decoder/ReferenceDimSuscriberCRMInf.java
@@ -0,0 +1,604 @@
+package com.gamma.skybase.build.server.etl.decoder;
+public class ReferenceDimSuscriberCRMInf {
+    String id, subId, custId, actualCustId, subType, subgroupType, subgroupName, networkType, prepaidFlag, msisdn, imsi, iccid, subPassword, subLan, subLevel, subGroup, subSegment, dunFlag, dunStartDate, dunExpiryDate, subInitCredit, subCredit, subState, subStateReason, agreementNo, createDate, agreementDate, firstEffDate, effDate, expDate, modDate, activeDate, latestActiveDate, createOperId, createLocalId, busiSeq, remark, partitionId, subInitCreditId, virtualSubId, cpeMac, reserveReconnTime, dealerId, info1, info2, info3, info4, info5, info6, info7, info8, info9, info10, info11, info12, relamsisdn, username, brandId, displayType, beId, salesOperId, salesDeptId, salesDealerId, salesChannel, createDeptId, parentSubId, externalSubId, subCode, writtenlLan, ownerOrgId, snType, subLoginPassword, roleType, initPwdChg, subClass;
+
+    public String getMsisdn() {
+        return msisdn;
+    }
+
+    public void setMsisdn(String msisdn) {
+        this.msisdn = msisdn;
+    }
+
+    public String getPrepaidFlag() {
+        return prepaidFlag;
+    }
+
+    public void setprepaidFlag(String prepaidFlag) {
+        this.prepaidFlag = prepaidFlag;
+    }
+
+    public String getCustId() {
+        return custId;
+    }
+
+    public void setCustId(String custId) {
+        this.custId = custId;
+    }
+
+    public String getActualCustId() {
+        return actualCustId;
+    }
+
+    public void setActualCustId(String actualCustId) {
+        this.actualCustId = actualCustId;
+    }
+
+    public String getSubType() {
+        return subType;
+    }
+
+    public void setSubType(String subType) {
+        this.subType = subType;
+    }
+
+    public String getSubgroupType() {
+        return subgroupType;
+    }
+
+    public void setSubgroupType(String subgroupType) {
+        this.subgroupType = subgroupType;
+    }
+
+    public String getSubgroupName() {
+        return subgroupName;
+    }
+
+    public void setSubgroupName(String subgroupName) {
+        this.subgroupName = subgroupName;
+    }
+
+    public String getNetworkType() {
+        return networkType;
+    }
+
+    public void setNetworkType(String networkType) {
+        this.networkType = networkType;
+    }
+
+    public String getImsi() {
+        return imsi;
+    }
+
+    public void setImsi(String imsi) {
+        this.imsi = imsi;
+    }
+
+    public String getIccid() {
+        return iccid;
+    }
+
+    public void setIccid(String iccid) {
+        this.iccid = iccid;
+    }
+
+    public String getSubPassword() {
+        return subPassword;
+    }
+
+    public void setSubPassword(String subPassword) {
+        this.subPassword = subPassword;
+    }
+
+    public String getSubLan() {
+        return subLan;
+    }
+
+    public void setSubLan(String subLan) {
+        this.subLan = subLan;
+    }
+
+    public String getSubLevel() {
+        return subLevel;
+    }
+
+    public void setSubLevel(String subLevel) {
+        this.subLevel = subLevel;
+    }
+
+    public String getSubGroup() {
+        return subGroup;
+    }
+
+    public void setSubGroup(String subGroup) {
+        this.subGroup = subGroup;
+    }
+
+    public String getSubSegment() {
+        return subSegment;
+    }
+
+    public void setSubSegment(String subSegment) {
+        this.subSegment = subSegment;
+    }
+
+    public String getDunFlag() {
+        return dunFlag;
+    }
+
+    public void setDunFlag(String dunFlag) {
+        this.dunFlag = dunFlag;
+    }
+
+    public String getDunStartDate() {
+        return dunStartDate;
+    }
+
+    public void setDunStartDate(String dunStartDate) {
+        this.dunStartDate = dunStartDate;
+    }
+
+    public String getDunExpiryDate() {
+        return dunExpiryDate;
+    }
+
+    public void setDunExpiryDate(String dunExpiryDate) {
+        this.dunExpiryDate = dunExpiryDate;
+    }
+
+    public String getSubInitCredit() {
+        return subInitCredit;
+    }
+
+    public void setSubInitCredit(String subInitCredit) {
+        this.subInitCredit = subInitCredit;
+    }
+
+    public String getSubCredit() {
+        return subCredit;
+    }
+
+    public void setSubCredit(String subCredit) {
+        this.subCredit = subCredit;
+    }
+
+    public String getSubState() {
+        return subState;
+    }
+
+    public void setSubState(String subState) {
+        this.subState = subState;
+    }
+
+    public String getSubStateReason() {
+        return subStateReason;
+    }
+
+    public void setSubStateReason(String subStateReason) {
+        this.subStateReason = subStateReason;
+    }
+
+    public String getAgreementNo() {
+        return agreementNo;
+    }
+
+    public void setAgreementNo(String agreementNo) {
+        this.agreementNo = agreementNo;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getAgreementDate() {
+        return agreementDate;
+    }
+
+    public void setAgreementDate(String agreementDate) {
+        this.agreementDate = agreementDate;
+    }
+
+    public String getFirstEffDate() {
+        return firstEffDate;
+    }
+
+    public void setFirstEffDate(String firstEffDate) {
+        this.firstEffDate = firstEffDate;
+    }
+
+    public String getEffDate() {
+        return effDate;
+    }
+
+    public void setEffDate(String effDate) {
+        this.effDate = effDate;
+    }
+
+    public String getExpDate() {
+        return expDate;
+    }
+
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    public String getModDate() {
+        return modDate;
+    }
+
+    public void setModDate(String modDate) {
+        this.modDate = modDate;
+    }
+
+    public String getActiveDate() {
+        return activeDate;
+    }
+
+    public void setActiveDate(String activeDate) {
+        this.activeDate = activeDate;
+    }
+
+    public String getLatestActiveDate() {
+        return latestActiveDate;
+    }
+
+    public void setLatestActiveDate(String latestActiveDate) {
+        this.latestActiveDate = latestActiveDate;
+    }
+
+    public String getCreateOperId() {
+        return createOperId;
+    }
+
+    public void setCreateOperId(String createOperId) {
+        this.createOperId = createOperId;
+    }
+
+    public String getCreateLocalId() {
+        return createLocalId;
+    }
+
+    public void setCreateLocalId(String createLocalId) {
+        this.createLocalId = createLocalId;
+    }
+
+    public String getBusiSeq() {
+        return busiSeq;
+    }
+
+    public void setBusiSeq(String busiSeq) {
+        this.busiSeq = busiSeq;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getPartitionId() {
+        return partitionId;
+    }
+
+    public void setPartitionId(String partitionId) {
+        this.partitionId = partitionId;
+    }
+
+    public String getSubInitCreditId() {
+        return subInitCreditId;
+    }
+
+    public void setSubInitCreditId(String subInitCreditId) {
+        this.subInitCreditId = subInitCreditId;
+    }
+
+    public String getSubId() {
+        return subId;
+    }
+
+    public void setSubId(String subId) {
+        this.subId = subId;
+    }
+
+    public String getVirtualSubId() {
+        return virtualSubId;
+    }
+
+    public void setVirtualSubId(String virtualSubId) {
+        this.virtualSubId = virtualSubId;
+    }
+
+    public String getCpeMac() {
+        return cpeMac;
+    }
+
+    public void setCpeMac(String cpeMac) {
+        this.cpeMac = cpeMac;
+    }
+
+    public String getReserveReconnTime() {
+        return reserveReconnTime;
+    }
+
+    public void setReserveReconnTime(String reserveReconnTime) {
+        this.reserveReconnTime = reserveReconnTime;
+    }
+
+    public String getDealerId() {
+        return dealerId;
+    }
+
+    public void setDealerId(String dealerId) {
+        this.dealerId = dealerId;
+    }
+
+    public String getInfo1() {
+        return info1;
+    }
+
+    public void setInfo1(String info1) {
+        this.info1 = info1;
+    }
+
+    public String getInfo2() {
+        return info2;
+    }
+
+    public void setInfo2(String info2) {
+        this.info2 = info2;
+    }
+
+    public String getInfo3() {
+        return info3;
+    }
+
+    public void setInfo3(String info3) {
+        this.info3 = info3;
+    }
+
+    public String getInfo4() {
+        return info4;
+    }
+
+    public void setInfo4(String info4) {
+        this.info4 = info4;
+    }
+
+    public String getInfo5() {
+        return info5;
+    }
+
+    public void setInfo5(String info5) {
+        this.info5 = info5;
+    }
+
+    public String getInfo6() {
+        return info6;
+    }
+
+    public void setInfo6(String info6) {
+        this.info6 = info6;
+    }
+
+    public String getInfo7() {
+        return info7;
+    }
+
+    public void setInfo7(String info7) {
+        this.info7 = info7;
+    }
+
+    public String getInfo8() {
+        return info8;
+    }
+
+    public void setInfo8(String info8) {
+        this.info8 = info8;
+    }
+
+    public String getInfo9() {
+        return info9;
+    }
+
+    public void setInfo9(String info9) {
+        this.info9 = info9;
+    }
+
+    public String getInfo10() {
+        return info10;
+    }
+
+    public void setInfo10(String info10) {
+        this.info10 = info10;
+    }
+
+    public String getInfo11() {
+        return info11;
+    }
+
+    public void setInfo11(String info11) {
+        this.info11 = info11;
+    }
+
+    public String getInfo12() {
+        return info12;
+    }
+
+    public void setInfo12(String info12) {
+        this.info12 = info12;
+    }
+
+    public String getRelamsisdn() {
+        return relamsisdn;
+    }
+
+    public void setRelamsisdn(String relamsisdn) {
+        this.relamsisdn = relamsisdn;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(String brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getDisplayType() {
+        return displayType;
+    }
+
+    public void setDisplayType(String displayType) {
+        this.displayType = displayType;
+    }
+
+    public String getBeId() {
+        return beId;
+    }
+
+    public void setBeId(String beId) {
+        this.beId = beId;
+    }
+
+    public String getSalesOperId() {
+        return salesOperId;
+    }
+
+    public void setSalesOperId(String salesOperId) {
+        this.salesOperId = salesOperId;
+    }
+
+    public String getSalesDeptId() {
+        return salesDeptId;
+    }
+
+    public void setSalesDeptId(String salesDeptId) {
+        this.salesDeptId = salesDeptId;
+    }
+
+    public String getSalesDealerId() {
+        return salesDealerId;
+    }
+
+    public void setSalesDealerId(String salesDealerId) {
+        this.salesDealerId = salesDealerId;
+    }
+
+    public String getSalesChannel() {
+        return salesChannel;
+    }
+
+    public void setSalesChannel(String salesChannel) {
+        this.salesChannel = salesChannel;
+    }
+
+    public String getCreateDeptId() {
+        return createDeptId;
+    }
+
+    public void setCreateDeptId(String createDeptId) {
+        this.createDeptId = createDeptId;
+    }
+
+    public String getParentSubId() {
+        return parentSubId;
+    }
+
+    public void setParentSubId(String parentSubId) {
+        this.parentSubId = parentSubId;
+    }
+
+    public String getExternalSubId() {
+        return externalSubId;
+    }
+
+    public void setExternalSubId(String externalSubId) {
+        this.externalSubId = externalSubId;
+    }
+
+    public String getSubCode() {
+        return subCode;
+    }
+
+    public void setSubCode(String subCode) {
+        this.subCode = subCode;
+    }
+
+    public String getWrittenlLan() {
+        return writtenlLan;
+    }
+
+    public void setWrittenlLan(String writtenlLan) {
+        this.writtenlLan = writtenlLan;
+    }
+
+    public String getOwnerOrgId() {
+        return ownerOrgId;
+    }
+
+    public void setOwnerOrgId(String ownerOrgId) {
+        this.ownerOrgId = ownerOrgId;
+    }
+
+    public String getSnType() {
+        return snType;
+    }
+
+    public void setSnType(String snType) {
+        this.snType = snType;
+    }
+
+    public String getSubLoginPassword() {
+        return subLoginPassword;
+    }
+
+    public void setSubLoginPassword(String subLoginPassword) {
+        this.subLoginPassword = subLoginPassword;
+    }
+
+    public String getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(String roleType) {
+        this.roleType = roleType;
+    }
+
+    public String getInitPwdChg() {
+        return initPwdChg;
+    }
+
+    public void setInitPwdChg(String initPwdChg) {
+        this.initPwdChg = initPwdChg;
+    }
+
+    public String getSubClass() {
+        return subClass;
+    }
+
+    public void setSubClass(String subClass) {
+        this.subClass = subClass;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
