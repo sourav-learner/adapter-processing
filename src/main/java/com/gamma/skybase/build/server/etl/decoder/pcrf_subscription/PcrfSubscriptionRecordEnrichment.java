1dbf019c90cbfb31651f1dae2e473132a4e9b3c2
PALLAVI DHAR <pallavi.dhar@gammanalytics.com>
2023-02-28 17:43:26 +0530

    PCRFSubscription Add

---
+++ src/main/java/com/gamma/skybase/build/server/etl/decoder/pcrf_subscription/PcrfSubscriptionRecordEnrichment.java
@@ -0,0 +1,46 @@
+package com.gamma.skybase.build.server.etl.decoder.pcrf_subscription;
+
+import com.gamma.skybase.contract.decoders.IEnrichment;
+import com.gamma.skybase.contract.decoders.MEnrichmentReq;
+import com.gamma.skybase.contract.decoders.MEnrichmentResponse;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashMap;
+import java.util.Optional;
+
+public class PcrfSubscriptionRecordEnrichment implements IEnrichment {
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss");
+
+    public MEnrichmentResponse transform(MEnrichmentReq request) {
+        MEnrichmentResponse response = new MEnrichmentResponse();
+        LinkedHashMap<String, Object> record = request.getRequest();
+
+        PcrfSubscriptionEnrichmentUtil tx = PcrfSubscriptionEnrichmentUtil.of(record);
+
+//        FILE_NAME , EVENT_DATE
+        record.put("FILE_NAME", record.get("fileName"));
+
+//        XDR_DATE
+        Optional<String> xdrDate = tx.getXdrDate();
+        xdrDate.ifPresent(s -> record.put("XDR_DATE", s));
+
+//        POPULATION_DATE
+        LocalDateTime ldt = LocalDateTime.now();
+        record.put("POPULATION_DATE",dtf.format(ldt));
+
+//        EVENT_DATE
+        Optional<String> eventDate = tx.getEventDate();
+        eventDate.ifPresent(s -> record.put("EVENT_DATE", s));
+
+        response.setResponseCode(true);
+        response.setResponse(record);
+        return response;
+    }
+
+    @Override
+    public LinkedHashMap<String, Object> transform(LinkedHashMap<String, Object> record) {
+        return record;
+    }
+}
